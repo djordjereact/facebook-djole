658b2f236288fb8eba706340f6d1b4f7b40b0ba7
Djole <djole07mihailovic@gmail.com>
2023-06-27 11:22:25 +0200

    Finished Header

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -1,6 +1,12 @@
 import Image from "next/image";
 import { FlagIcon, MagnifyingGlassIcon, PlayIcon, ShoppingCartIcon } from "@heroicons/react/24/outline";
-import { HomeIcon, UserGroupIcon } from "@heroicons/react/20/solid";
+import {
+    BellIcon,
+    ChatBubbleOvalLeftEllipsisIcon,
+    ChevronDownIcon,
+    HomeIcon, Squares2X2Icon,
+    UserGroupIcon
+} from "@heroicons/react/20/solid";
 import HeaderIcon from "@/app/components/HeaderIcon";
 
 function Header() {
@@ -13,7 +19,8 @@ function Header() {
                 <div className='flex ml-2 rounded-full bg-gray-100 p-2'>
                     <MagnifyingGlassIcon className='h-6 text-gray-600' />
                     <input
-                        className='flex ml-2 items-center outline-none placeholder-gray-500 bg-transparent flex-shrink'
+                        className='hidden md:inline-flex ml-2 items-center outline-none
+                        placeholder-gray-500 bg-transparent flex-shrink'
                         type="text"
                         placeholder='Search Facebook'/>
                 </div>
@@ -30,6 +37,17 @@ function Header() {
                 </div>
             </div>
 
+
+            {/*Right*/}
+            <div className='flex items-center sm: space-x-2 justify-end'>
+                {/*Profile pic*/}
+
+                <p className='font-semibold pr-3 whitespace-nowrap'>Djole</p>
+                <Squares2X2Icon className='icon'/>
+                <ChatBubbleOvalLeftEllipsisIcon className='icon' />
+                <BellIcon  className='icon'/>
+                <ChevronDownIcon className='icon' />
+            </div>
 
         </div>
     )
